cbeb079766e7fe4f3d3fc1e9de1205da8d136bed
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-11-22 22:09:13 -0300

    BIGDATA so much files

--- aula002_HiveEImpala.sh
+++ aula002_HiveEImpala.sh
@@ -1,3 +1,9 @@
 #Hive
 
 $hive
+
+https://gitlab.com/vmb1/hive
+
+https://drive.google.com/file/d/1duwf7g9lfAsIOJWRL26tx8RBfV8ySPon/view
+
+https://drive.google.com/file/d/1CsHc311jp4EuZ8be5KGaumniGAafa8sC/view
